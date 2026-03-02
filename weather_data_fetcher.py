2d06e0ee95638a53fb905fe9388892272d6cb9fb
u_38015187 IES MONTAÑA DE GUAZA <u_38015187@medusa.gobiernodecanarias.net>
2026-03-02 14:08:46 +0000

    Temperatura y humedad

--- weather_data_fetcher.py
+++ weather_data_fetcher.py
@@ -1,15 +1,44 @@
 import requests
+import openmeteo_requests
 
-url = "https://api.waqi.info/feed/@6779/?token=db94e4c768d260335021f2dbc8dfc088345d6bab"
-url2 = "https://api.thingspeak.com/update?api_key=BTCDYRXPGC0PZOWK&field1="
+openmeteo = openmeteo_requests.Client()
 
-res = requests.get(url)
+def GetAirQuality():
+    global air_quality, latitude, longitude
+    url = "https://api.waqi.info/feed/@6779/?token=db94e4c768d260335021f2dbc8dfc088345d6bab"
+    res = requests.get(url)
+    if res.status_code == 200:
+        data = res.json()
+        air_quality = data["data"]["aqi"]
+        latitude = int(data["data"]["city"]["geo"][0])
+        longitude = int(data["data"]["city"]["geo"][1])
+        print("Calidad del aire:", air_quality)
+    else:
+        print("Error fetching air quality:", res.status_code)
 
-if res.status_code == 200:
-    data = res.json()
-    air = data["data"]["aqi"]
-    print("Calidad del aire:", air)
-    res2 = requests.get(url2 + str(air))
-    print(res.status_code)
-else:
-    print("Error:", res.status_code)
+def GetTemperatureAndHumidity():
+    global temperature, humidity
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": ["temperature_2m", "relative_humidity_2m"]
+    }
+    res = openmeteo.weather_api(url, params=params)
+    current = res[0].Current()
+    temperature = round(current.Variables(0).Value())
+    humidity = round(current.Variables(1).Value())
+    print("Temperatura:", str(temperature) + " C")
+    print("Humedad:", str(humidity) + "%")
+
+def UpdateGraph():
+    url = f"https://api.thingspeak.com/update?api_key=BTCDYRXPGC0PZOWK&field1={air_quality}&field2={temperature}&field3={humidity}"
+    res = requests.get(url)
+    if res.status_code == 200:
+        print("Success")
+    else:
+        print("Error updating data:", res.status_code)
+
+GetAirQuality()
+GetTemperatureAndHumidity()
+UpdateGraph()
