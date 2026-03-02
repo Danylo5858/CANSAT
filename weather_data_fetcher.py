31d8ed70eae562a978073c43d3405da6e9c60417
u_38015187 IES MONTAÑA DE GUAZA <u_38015187@medusa.gobiernodecanarias.net>
2026-03-02 12:25:38 +0000

    Integracion de Thingspeak

--- weather_data_fetcher.py
+++ weather_data_fetcher.py
@@ -1,11 +1,15 @@
 import requests
 
-url = "https://api.waqi.info/feed/@6753/?token=db94e4c768d260335021f2dbc8dfc088345d6bab"
+url = "https://api.waqi.info/feed/@6779/?token=db94e4c768d260335021f2dbc8dfc088345d6bab"
+url2 = "https://api.thingspeak.com/update?api_key=BTCDYRXPGC0PZOWK&field1="
 
 res = requests.get(url)
 
 if res.status_code == 200:
     data = res.json()
-    print("Calidad del aire:", data["data"]["aqi"])
+    air = data["data"]["aqi"]
+    print("Calidad del aire:", air)
+    res2 = requests.get(url2 + str(air))
+    print(res.status_code)
 else:
     print("Error:", res.status_code)
