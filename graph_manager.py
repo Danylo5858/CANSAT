364b583b770d55c7e7966fca87d7653e9fe3226c
Danylo <smolyakdanylo@gmail.com>
2026-03-04 14:09:58 +0000

    Creando el archivo graph_manager.py

---
+++ graph_manager.py
@@ -0,0 +1,9 @@
+import requests
+
+def UpdateGraph():
+    url = f"https://api.thingspeak.com/update?api_key=BTCDYRXPGC0PZOWK&field1={air_quality}&field2={temperature}&field3={humidity}&field4={altitude_cansat}&field5={temperature_cansat}&field6={pressure_cansat}"
+    res = requests.get(url)
+    if res.status_code == 200:
+        print("Success")
+    else:
+        print("Error updating data:", res.status_code)
