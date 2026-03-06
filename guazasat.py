c21dede733c90c7ba40ec8717877d81651f27a6a
u_38015187 IES MONTAÑA DE GUAZA <u_38015187@medusa.gobiernodecanarias.net>
2026-03-06 13:38:07 +0000

    Añadiendo Threads

--- guazasat.py
+++ guazasat.py
@@ -1,6 +1,7 @@
 import Modules.BMP390 as bmp
 import Modules.MPU6050 as mpu
 import Modules.GPS as gps
+import threading
 import board
 import busio
 import RPi.GPIO as GPIO
@@ -10,20 +11,21 @@ GlobalSleepTime = 1
 i2c = busio.I2C(board.SCL, board.SDA)
 print("Devices: ", [hex(device_address) for device_address in i2c.scan()])
 
-mpu.init(i2c, 0x68)
 mpu.SleepTime = GlobalSleepTime
 mpu.log = True
-mpu.SaveData(GlobalSleepTime)
+mpu.init(i2c, 0x68)
 
-bmp.init(i2c, 0x76)
 bmp.SleepTime = GlobalSleepTime
 bmp.log = True
-bmp.SaveData(GlobalSleepTime)
+bmp.init(i2c, 0x76)
 
-gps.init(i2c, 0x10, True)
 gps.SleepTime = GlobalSleepTime
 gps.log = True
-gps.SaveData(GlobalSleepTime)
+gps.init(i2c, 0x10)
+
+mpu_thread = threading.Thread(target=mpu.SaveData, args=(GlobalSleepTime), daemon=True)
+bmp_thread = threading.Thread(target=bmp.SaveData, args=(GlobalSleepTime), daemon=True)
+gps_thread = threading.Thread(target=gps.SaveData, args=(GlobalSleepTime), daemon=True)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT) # LED
