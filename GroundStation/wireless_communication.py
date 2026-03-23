baf66e737649874c703cddb9b56e414257149d15
Danylo <smolyakdanylo@gmail.com>
2026-03-23 17:07:07 +0000

    Implementando y mejorando sx126x.py

--- GroundStation/wireless_communication.py
+++ GroundStation/wireless_communication.py
@@ -1,18 +1,29 @@
-import threading
-from sx126x import SX126X, Address
+#import threading
+#from sx126x import SX126X, Address
 
-log = False
+#log = False
 
-print_lock = threading.Lock()
+#print_lock = threading.Lock()
 
-def start():
-    print("init")
-    lora = SX126X(Address(1, 1))
-    while True:
-        print("TEST")
-        #addr, data = lora.rx()
-        if log:
-            with print_lock:
-                print(f"Recibido de {addr}: {data}")
+#def start():
+#    print("init")
+#    lora = SX126X(Address(1, 1))
+#    while True:
+#        print("TEST")
+#        addr, data = lora.rx()
+#        if log:
+#            with print_lock:
+#                print(f"Recibido de {addr}: {data}")
 
-start()
+#start()
+
+import sx126x
+import time
+
+lora = sx126x.sx126x("/dev/ttyS0", freq=868, addr=2, power=22)
+
+while True:
+    data = lora.receive()
+    if data:
+        print("Recibido:", data)
+    time.sleep(0.5)
--- wireless_communication_cansat.py
+++ wireless_communication_cansat.py
@@ -1,16 +1,26 @@
-from sx126x import SX126X, Address
+#from sx126x import SX126X, Address
 
-print("a")
-lora = SX126X(Address(1, 2))
-receiver = Address(1, 1)
-print("b", flush=True)
+#print("a")
+#lora = SX126X(Address(1, 2))
+#receiver = Address(1, 1)
+#print("b", flush=True)
 #lora.tx(receiver, "1234")
-print("c")
+#print("c")
 
-def send(data):
-    print("SENDING...")
-    lora = SX126X(Address(1, 2))
-    receiver = Address(1, 1)
-    lora.tx(receiver, data)
-    print("SENT")
-    return f"Enviado a {receiver}: {data.decode()}"
+#def send(data):
+#    print("SENDING...")
+#    lora = SX126X(Address(1, 2))
+#    receiver = Address(1, 1)
+#    lora.tx(receiver, data)
+#    print("SENT")
+#    return f"Enviado a {receiver}: {data.decode()}"
+
+import sx126x
+import time
+
+lora = sx126x.sx126x("/dev/ttyS0", freq=868, addr=1, power=22)
+
+while True:
+    lora.send("2,868,CANSAT")
+    print("Enviado")
+    time.sleep(3)
