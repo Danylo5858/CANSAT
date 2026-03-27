a26110ac849b3a7c3a23f3e552ca8918dbf7f7f6
Danylo <smolyakdanylo@gmail.com>
2026-03-27 21:19:44 +0000

    _

--- testing.py
+++ testing.py
@@ -1,70 +1,13 @@
-import spidev
-import RPi.GPIO as GPIO
+import serial
 import time
 
-# Pines (AJUSTA según tu wiring real)
-RESET_PIN = 22
-BUSY_PIN = 17
+ser = serial.Serial("/dev/serial0", 9600, timeout=1)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(RESET_PIN, GPIO.OUT)
-GPIO.setup(BUSY_PIN, GPIO.IN)
+print("Probando UART...")
 
-spi = spidev.SpiDev()
-spi.open(0, 0)
-spi.max_speed_hz = 1000000
-spi.mode = 0
+ser.write(bytes([0xC1, 0x00, 0x09]))  # get config
+time.sleep(0.5)
 
-# -------------------------
-# RESET del chip
-# -------------------------
-def reset_chip():
-    GPIO.output(RESET_PIN, GPIO.LOW)
-    time.sleep(0.01)
-    GPIO.output(RESET_PIN, GPIO.HIGH)
-    time.sleep(0.1)
+data = ser.read(ser.in_waiting or 1)
 
-# -------------------------
-# Esperar BUSY libre
-# -------------------------
-def wait_busy():
-    while GPIO.input(BUSY_PIN) == 1:
-        time.sleep(0.001)
-
-# -------------------------
-# Enviar comando SX126x
-# -------------------------
-def sx_write(cmd):
-    wait_busy()
-    spi.xfer2(cmd)
-    wait_busy()
-
-# -------------------------
-# Leer respuesta simple
-# -------------------------
-def sx_read(cmd, length=2):
-    wait_busy()
-    spi.xfer2(cmd)
-    wait_busy()
-    return spi.xfer2([0x00]*length)
-
-# -------------------------
-# TEST
-# -------------------------
-print("Resetting SX126X...")
-reset_chip()
-
-print("Probando comunicación...")
-
-# GetStatus = 0xC0
-resp = sx_read([0xC0, 0x00], 2)
-
-print("STATUS:", resp)
-
-# GetDeviceErrors = 0xC3
-resp2 = sx_read([0xC3, 0x00, 0x00], 3)
-
-print("ERRORS:", resp2)
-
-spi.close()
-GPIO.cleanup()
+print("Respuesta:", data)
