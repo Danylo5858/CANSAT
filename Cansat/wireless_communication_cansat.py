b9b2a731883f28707bde33235de3934de8fe1b44
Danylo <smolyakdanylo@gmail.com>
2026-03-27 23:31:59 +0000

    _

--- Cansat/wireless_communication_cansat.py
+++ Cansat/wireless_communication_cansat.py
@@ -4,11 +4,12 @@ from queue import Queue
 radio = sx126x("/dev/serial0", 868, 0x0001, 22, True)
 dest_addr = 0x0002
 msg_queue = Queue()
-print("WIRELEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEES INIT")
+
+print("WIRELEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEESS INIT")
 
 def send(str_msg):
-	msg = str_msg.encode("utf-8") 
-	dest_h = (dest_addr >> 8) & 0xFF
+    msg = str_msg.encode("utf-8")
+    dest_h = (dest_addr >> 8) & 0xFF
     dest_l = dest_addr & 0xFF
     src_h = (radio.addr >> 8) & 0xFF
     src_l = radio.addr & 0xFF
@@ -20,6 +21,6 @@ def send(str_msg):
     radio.send(data)
 
 def sender():
-	while True:
-		msg = msg_queue.get()
-		send(msg)
+    while True:
+        msg = msg_queue.get()
+        send(msg)
