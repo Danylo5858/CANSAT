a8d094f7078aea5989b104dd2ed524ff6d31a68f
Danylo <smolyakdanylo@gmail.com>
2026-03-28 17:41:10 +0000

    _

--- GroundStation/wireless_communication_gs.py
+++ GroundStation/wireless_communication_gs.py
@@ -1,5 +1,6 @@
 import time
 import json
+import gzip
 from sx126x import sx126x
 from log_manager import log_queue
 
@@ -7,7 +8,8 @@ log = False
 
 radio = sx126x("/dev/serial0", 868, 2, 22, False)
 
-def OnReceive(str_data):
+def OnReceive(raw_data):
+	str_data = gzip.decompress(raw_data).decode("utf-8")
 	if log:
 		log_queue.put(str_data)
 	#data = json.loads(str_data)
