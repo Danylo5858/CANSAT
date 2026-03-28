11d64cb80ec796cb924860f1ea1d117e51598ffc
Danylo <smolyakdanylo@gmail.com>
2026-03-28 16:39:13 +0000

    _

--- GroundStation/wireless_communication_gs.py
+++ GroundStation/wireless_communication_gs.py
@@ -10,7 +10,7 @@ radio = sx126x("/dev/serial0", 868, 2, 22, False)
 def OnReceive(str_data):
 	if log:
 		log_queue.put(str_data)
-	data = json.loads(str_data)
+	#data = json.loads(str_data)
 
 def receiver():
 	while True:
