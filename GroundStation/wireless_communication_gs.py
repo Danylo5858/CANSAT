056433f89e5c576ecbaf84527f51af804e443c58
Danylo <smolyakdanylo@gmail.com>
2026-03-28 00:32:44 +0000

    _

--- GroundStation/wireless_communication_gs.py
+++ GroundStation/wireless_communication_gs.py
@@ -17,13 +17,13 @@
 
 #start()
 
-import sx126x as sx126x
-import time
+#import sx126x as sx126x
+#import time
 
-lora = sx126x.sx126x("/dev/ttyS0", freq=868, addr=2, power=22)
+#lora = sx126x.sx126x("/dev/ttyS0", freq=868, addr=2, power=22)
 
-while True:
-    data = lora.receive()
-    if data:
-        print("Recibido:", data)
-    time.sleep(0.5)
+#while True:
+#    data = lora.receive()
+#    if data:
+#        print("Recibido:", data)
+#    time.sleep(0.5)
