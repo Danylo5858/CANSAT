df853b48454c5ebc3a5a020f6cefcd3624985276
Danylo <smolyakdanylo@gmail.com>
2026-03-28 14:53:54 +0000

    _

--- GroundStation/ground_station.py
+++ GroundStation/ground_station.py
@@ -1,12 +1,8 @@
-import time
-import json
 import threading
-from sx126x import sx126x
 import wireless_communication_gs as wcom_gs
 import graph_manager as gm
 import weather_data_fetcher as wdf
 import log_manager as lm
-from log_manager import log_queue
 
 threading.Thread(target=lm.logger, daemon=True).start()
 
@@ -20,13 +16,4 @@ gm.SleepTime = GlobalSleepTime
 gm.log = True
 #threading.Thread(target=gm.start, daemon=False).start()
 
-#threading.Thread(target=wcom.start, daemon=False).start()
-
-def OnReceive(data):
-	log_queue.put(data)
-
-radio = sx126x("/dev/serial0", 868, 2, 22, False)
-
-while True:
-	radio.receive(OnReceive)
-	time.sleep(0.5)
+threading.Thread(target=wcom_gs.receiver, daemon=False).start()
--- GroundStation/wireless_communication_gs.py
+++ GroundStation/wireless_communication_gs.py
@@ -1,29 +1,14 @@
-#import threading
-#from sx126x import SX126X, Address
+import time
+import json
+from sx126x import sx126x
+from log_manager import log_queue
 
-#log = False
+radio = sx126x("/dev/serial0", 868, 2, 22, False)
 
-#print_lock = threading.Lock()
+def OnReceive(data):
+	log_queue.put(data)
 
-#def start():
-#    print("init")
-#    lora = SX126X(Address(1, 1))
-#    while True:
-#        print("TEST")
-#        addr, data = lora.rx()
-#        if log:
-#            with print_lock:
-#                print(f"Recibido de {addr}: {data}")
-
-#start()
-
-#import sx126x as sx126x
-#import time
-
-#lora = sx126x.sx126x("/dev/ttyS0", freq=868, addr=2, power=22)
-
-#while True:
-#    data = lora.receive()
-#    if data:
-#        print("Recibido:", data)
-#    time.sleep(0.5)
+def receiver():
+	while True:
+		radio.receive(OnReceive)
+		time.sleep(0.5)
