8d636cf8d13659cc8f6330121dd8706998e92f5a
Danylo <smolyakdanylo@gmail.com>
2026-03-29 02:56:03 +0100

    Implementando websocket y mejorando el index.html

--- GroundStation/Server/app.py
+++ GroundStation/Server/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify, render_template
+from flask_socketio import SocketIO
+import eventlet
+from log_manager import log_queue
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+def SendData(data):
+	socketio.emit("BMP390_data", data)
 
 @app.route("/")
 def index():
 	return render_template("index.html")
 
+@socketio.on("connect")
+def handle_connect():
+	log_queue.put("Cliente conectado")
+
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000, debug=True)
+	socketio.run(app, host="0.0.0.0", port=5000)
--- GroundStation/ground_station.py
+++ GroundStation/ground_station.py
@@ -4,6 +4,7 @@ import graph_manager as gm
 import weather_data_fetcher as wdf
 import log_manager as lm
 import wireless_communication_gs as wcom_gs
+from Server.app import SendData
 
 threading.Thread(target=lm.logger, daemon=True).start()
 
@@ -17,6 +18,7 @@ gm.log = True
 while True:
 	data = wcom_gs.received_data.get()
 	threads = [
+		threading.Thread(target=SendData, args=(data["BMP390"],), daemon=True),
 		threading.Thread(target=gm.UpdateGraph, args=(data["BMP390"],), daemon=True)
 	]
 	for t in threads:
