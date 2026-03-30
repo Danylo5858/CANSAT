8f37f54de9a0eeba3a6b0241560ceff8fcd55dc2
Danylo <smolyakdanylo@gmail.com>
2026-03-30 19:50:01 +0100

    Agregando un loader y reestructurando todas las charts

--- GroundStation/Server/static/js/app.js
+++ GroundStation/Server/static/js/app.js
@@ -1,179 +1,87 @@
-// CHART.JS
-const WINDOW = 60 * 1000;
-
-function createChart(ctx, label, color, yLabel) {
-	return new Chart(ctx, {
-		type: 'line',
-		data: {
-			datasets: [{
-				label: label,
-				data: [],
-				borderColor: color,
-				tension: 0.4,
-		   		//cubicInterpolationMode: 'monotone',
-		  		//borderWidth: 2
-			}]
-		},
-		options: {
-			responsive: true,
-			//animation: {
-	    	//	duration: 1000,
-	  		//},
-			scales: {
-				x: {
-					type: 'time',
-					//time: {
-					//	unit: 'second',
-					//	displayFormats: {
-					//		second: 'HH:mm:ss',
-					//		minute: 'HH:mm',
-					//		hour: 'HH:mm'
-					//	}
-					//},
-					//ticks: {
-					//	callback: function(value) {
-					//		return new Date(value).toLocaleTimeString('es-ES', {
-					//			hour: '2-digit',
-					//			minute: '2-digit',
-					//			second: '2-digit',
-					//			hour12: false
-					//		});
-					//	}
-					//},
-					title: {
-						display: true,
-						text: 'Tiempo'
-					},
-					offset: true
-				},
-				y: {
-					title: {
-						display: true,
-						text: yLabel
-					}
-				}
-			},
-			layout: {
-	    		padding: {
-	      			left: 10,
-	      			right: 10
-	    		}
-	  		},
-	  		plugins: {
-				tooltip: {
-					callbacks: {
-						title: (items) => {
-							const date = new Date(items[0].parsed.x);
-							return date.toLocaleTimeString('es-ES', {
-								year: 'numeric',
-								month: '2-digit',
-								day: '2-digit',
-								hour: '2-digit',
-								minute: '2-digit',
-								second: '2-digit',
-								hour12: false
-							});
-						}
-					}
-				}
-			}
-		}
-	});
-}
-
-const charts = [
-	//createChart(document.getElementById('chart_a'), 'Altitud del CanSat', 'rgb(75, 192, 192)', 'Altitud (m)'),
-	//createChart(document.getElementById('chart_t'), 'Temperatura del CanSat', 'rgb(75, 192, 75)', 'Temperatura (Celsius)'),
-	//createChart(document.getElementById('chart_p'), 'Presión del CanSat', 'rgb(75, 75, 192)', 'Presión (hPa)')
-];
-
-let lastDate = 0;
-let data = [];
-let TICKINTERVAL = 1000;
-let XAXISRANGE = 60 * 1000;
-
-function getNewSeries(baseval, yrange) {
-	let newDate = baseval + TICKINTERVAL;
-	lastDate = newDate;
-	for (let i = 0; i < data.length - 10; i++) {
-		data[i].x = newDate - XAXISRANGE - TICKINTERVAL;
-		data[i].y = 0;
-	}
-	data.push({
-		x: newDate,
-		y: Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min
-	});
-}
+const WINDOW_SIZE = 60 * 1000;
 
 const options = {
-	series: [{
-		data: []
-	}],
 	chart: {
 		type: 'line',
 		animations: {
 			enabled: true,
+			easing: 'linear',
 			dynamicAnimation: {
-				speed: 1000
+				speed: 1000 // SINCRONIZAR CON LOS DATOS
+			},
+			animateGradually: {
+				enabled: false
 			}
 		},
 		toolbar: {
 			show: false
+		},
+		zoom: {
+			enabled: false
 		}
 	},
+	series: [{
+		name: 'Altitud',
+		data: []
+	}],
 	xaxis: {
 		type: 'datetime',
-		range: XAXISRANGE
+		range: WINDOW_SIZE,
+		tickAmount: 3,
+		labels: {
+			datetimeUTC: false,
+			formatter: function (value, timestamp) {
+				const d = new Date(timestamp);
+				return d.toLocaleTimeString([], {
+					hour: '2-digit',
+					minute: '2-digit',
+					second: '2-digit'
+				});
+			}
+		}
 	},
 	yaxis: {
-		max: 100
+		title: {
+			text: 'Altitud (m)'
+		}
+	},
+	stroke: {
+		curve: 'smooth',
+		width: 2
 	}
 };
 
-const test = new ApexCharts(document.querySelector('#chart'), options);
-test.render();
+const chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
 
-setInterval(function () {
-	getNewSeries(lastDate, { min: 10, max: 90 });
-	test.updateSeries([{ data: data }]);
+function getNewPoint() {
+	return {
+		x: new Date().getTime(),
+		y: Math.floor(Math.random() * 1000) + 100
+	};
+}
+
+setInterval(() => {
+	chart.appendData([{
+		data: [getNewPoint()]
+	}]);
 }, 1000);
 
-// SOCKETIO
-const socket = io("http://localhost:5000");
 
-socket.on("connect", () => {
-	console.log("Conectado al servidor");
+const socket = io('http://localhost:5000');
+let firstData = true
+const loadingTime = 3.5 * 1000;
+
+socket.on('connect', () => {
+	console.log('Conectado al servidor');
 });
 
-socket.on("BMP390_data", (data) => {
-	console.log("Datos recibidos:", data);
-
-	const now = new Date();
-	const altitude = data['altitude'];
-	const temperature = data['temperature'];
-	const pressure = data['pressure'];
-
-	//series[0].data.push({
-	//	x: now,
-	//	y: data.altitude
-	//});
-
-	//test.updateSeries(series);
-
-	//charts[0].data.datasets[0].data.push({
-	//	x: now,
-	//	y: altitude
-	//});
-	//charts[1].data.datasets[0].data.push({
-	//	x: now,
-	//	y: temperature
-	//});
-	//charts[2].data.datasets[0].data.push({
-	//	x: now,
-	//	y: pressure
-	//});
-
-	//for (const chart of charts) {
-	//	chart.update('none');
-	//}
+socket.on('BMP390_data', (data) => {
+	console.log('Datos recibidos:', data);
+	if (firstData) {
+		setTimeout(() => {
+			document.querySelector('.loader-container').classList.add('hide');
+		}, loadingTime);
+		firstData = false;
+	}
 });
