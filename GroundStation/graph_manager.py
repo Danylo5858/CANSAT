852bbee66524c43ce60335daed3b33dea7ea84cb
Danylo <smolyakdanylo@gmail.com>
2026-04-01 01:57:02 +0100

    _

--- GroundStation/graph_manager.py
+++ GroundStation/graph_manager.py
@@ -3,6 +3,7 @@ import requests
 from log_manager import log_queue
 
 log = False
+
 def update_graph(name, data):
     if name == "satellite":
         a = data["altitude"]
@@ -14,16 +15,18 @@ def update_graph(name, data):
         t = data["temperature"]
         h = data["humidity"]
         url = f"https://api.thingspeak.com/update?api_key=BTCDYRXPGC0PZOWK&field1={aq}&field2={t}&field3={h}"
-        requests.get(f"https://api.thingspeak.com/update?api_key=BTCDYRXPGC0PZOWK&field1={aq}&field2={t}&field3={h}")
-    #try:
-        #res = requests.get(url)
-    #except Exception as e:
-    #    if log:
-    #        log_queue.put(f"update_graph [{name}] error: {e}")
-    #if res and res.status_code == 200 and log:
-    #    log_queue.put(f"Graficos [{name}] actualizados")
-    #elif log:
-    #    if res:
-    #        log_queue.put(f"Error actualizando graficos [{name}]: {res.status_code}")
-    #    else:
-    #        log_queue.put(f"Error actualizando graficos [{name}]")
+    try:
+        res = requests.get(url)
+    except Exception as e:
+        if log:
+            log_queue.put(f"update_graph [{name}] error: {e}")
+    if res and res.status_code == 200 and log:
+        log_queue.put(f"Graficos [{name}] actualizados")
+        url = "https://api.thingspeak.com/channels/3283442/status.json"
+        res = requests.get(url)
+        log_queue.put(res.json())
+    elif log:
+        if res:
+            log_queue.put(f"Error actualizando graficos [{name}]: {res.status_code}")
+        else:
+            log_queue.put(f"Error actualizando graficos [{name}]")
