8baa5d31a46871593d75d2344cd1124960d53fbf
Danylo <smolyakdanylo@gmail.com>
2026-04-02 23:33:48 +0100

    Cambiando API de GetAirQuality()

--- GroundStation/weather_data_fetcher.py
+++ GroundStation/weather_data_fetcher.py
@@ -7,7 +7,7 @@ log = False
 lat = 0
 lon = 0
 
-token = "db94e4c768d260335021f2dbc8dfc088345d6bab"
+# token = "db94e4c768d260335021f2dbc8dfc088345d6bab"
 
 def init():
     global openmeteo
@@ -17,35 +17,62 @@ def init():
         if log:
             log_queue.put(f"weather_data_fetcher init error: {e}")
 
+# def GetAirQuality():
+#     if lat == 0 and lon == 0:
+#         url = f"https://api.waqi.info/feed/@6779/?token={token}"
+#     else:
+#         url = f"https://api.waqi.info/feed/geo:{lat};{lon}/?token={token}"
+#     try:
+#         res = requests.get(url)
+#     except Exception as e:
+#         if log:
+#             log_queue.put(f"GetAirQuality Error: {e}")
+#     if res and res.status_code == 200:
+#         data = res.json()
+#         if data["status"] == "ok":
+#             air_quality = data["data"]["aqi"]
+#             #lat = int(data["data"]["city"]["geo"][0])
+#             #lon = int(data["data"]["city"]["geo"][1])
+#             air_quality = air_quality
+#             if log:
+#                 log_queue.put(f"Calidad del aire: {air_quality}")
+#             return air_quality
+#         else:
+#             if log:
+#                 log_queue.put(f"API Error fetching air quality: {data['message']}")
+#     else:
+#         if log:
+#             if res:
+#                 log_queue.put(f"HTTP Error fetching air quality: {res.status_code}")
+#             else:
+#                 log_queue.put("HTTP Error fetching air quality")
+#     return None
+
 def GetAirQuality():
-    if lat == 0 and lon == 0:
-        url = f"https://api.waqi.info/feed/@6779/?token={token}"
-    else:
-        url = f"https://api.waqi.info/feed/geo:{lat};{lon}/?token={token}"
+    url = "https://air-quality-api.open-meteo.com/v1/air-quality"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "current": ["us_aqi"]
+    }
     try:
-        res = requests.get(url)
+        res = openmeteo.weather_api(url, params=params)
+        if not res:
+            if log:
+                log_queue.put("GetAirQuality Error: 'res' is None")
+            return None
+        current = res[0].Current()
+        if not current:
+            if log:
+                log_queue.put("GetAirQuality Error: 'current' is None")
+            return None
+        aqi = round(current.Variables(0).Value())
+        if log:
+            log_queue.put(f"AQI: {aqi}")
+        return aqi
     except Exception as e:
         if log:
             log_queue.put(f"GetAirQuality Error: {e}")
-    if res and res.status_code == 200:
-        data = res.json()
-        if data["status"] == "ok":
-            air_quality = data["data"]["aqi"]
-            #lat = int(data["data"]["city"]["geo"][0])
-            #lon = int(data["data"]["city"]["geo"][1])
-            air_quality = air_quality
-            if log:
-                log_queue.put(f"Calidad del aire: {air_quality}")
-            return air_quality
-        else:
-            if log:
-                log_queue.put(f"API Error fetching air quality: {data['message']}")
-    else:
-        if log:
-            if res:
-                log_queue.put(f"HTTP Error fetching air quality: {res.status_code}")
-            else:
-                log_queue.put("HTTP Error fetching air quality")
     return None
 
 def GetTemperatureAndHumidity():
@@ -79,8 +106,7 @@ def GetTemperatureAndHumidity():
     return None, None
 
 def fetch():
-    # air_quality = GetAirQuality()
-    air_quality = None
+    air_quality = GetAirQuality()
     temperature, humidity = GetTemperatureAndHumidity()
     return {
         "air_quality": air_quality,
