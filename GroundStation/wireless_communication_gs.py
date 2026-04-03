48256b462d4fc33b4bf5be965c8c1b67eb5a6158
Danylo <smolyakdanylo@gmail.com>
2026-04-03 22:19:44 +0100

    _

--- GroundStation/wireless_communication_gs.py
+++ GroundStation/wireless_communication_gs.py
@@ -9,25 +9,25 @@ log = False
 received_data = Queue()
 
 def init(self_address, frequency):
-    global radio
-    radio = sx126x("/dev/serial0", frequency, self_address, 22, False)
+	global radio
+	radio = sx126x("/dev/serial0", frequency, self_address, 22, False)
 
 def unpack_all(packet):
-    unpacked = struct.unpack('<iiB hhh h 16h', packet)
-    lat = unpacked[0] / 1e7
-    lon = unpacked[1] / 1e7
-    sats = unpacked[2]
-    temp = unpacked[3] / 100
-    pressure = unpacked[4] / 10
-    altitude = unpacked[5] / 10
-    quats_raw = unpacked[7:]
-    quats = [quats_raw[i:i+4] for i in range(0, 16, 4)]
-    quats = [[round(x / 32767, 5) for x in q] for q in quats]
-    return {
-        "GPS": {"latitude": lat, "longitude": lon, "satellites": sats},
-        "BMP390": {"temperature": temp, "pressure": pressure, "altitude": altitude},
-        "MPU6050": quats
-    }
+	unpacked = struct.unpack('<iiB hhh h 16h', packet)
+	lat = unpacked[0] / 1e7
+	lon = unpacked[1] / 1e7
+	sats = unpacked[2]
+	temp = unpacked[3] / 100
+	pressure = unpacked[4] / 10
+	altitude = unpacked[5] / 10
+	quats_raw = unpacked[7:]
+	quats = [quats_raw[i:i+4] for i in range(0, 16, 4)]
+	quats = [[round(x / 32767, 5) for x in q] for q in quats]
+	return {
+		"GPS": {"latitude": lat, "longitude": lon, "satellites": sats},
+		"BMP390": {"temperature": temp, "pressure": pressure, "altitude": altitude},
+		"MPU6050": quats
+	}
 
 def on_receive(packet):
 	data = unpack_all(packet)
@@ -36,5 +36,5 @@ def on_receive(packet):
 		log_queue.put(f"Datos recibidos: {data}")
 
 def receiver():
-    while True:
-	   radio.receive(on_receive)
+	while True:
+		radio.receive(on_receive)
