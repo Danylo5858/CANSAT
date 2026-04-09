802ace6df58fc8fe48d51b458aa7f29fcadc1abf
Danylo <smolyakdanylo@gmail.com>
2026-04-09 12:40:54 +0100

    _

--- Satellite/camera_controller.py
+++ Satellite/camera_controller.py
@@ -1,5 +1,4 @@
 import time
-import subprocess
 from datetime import datetime
 from picamera2 import Picamera2
 from log_manager import log_queue
@@ -16,7 +15,7 @@ def init(size):
 def capture():
     while True:
         try:
-            filename = f"Pictures/{time.time()}.jpg"
+            filename = f"Pictures/{datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}.jpg"
             picam2.capture_file(filename)
             if log:
                 log_queue.put(f"Imagen guardada: {filename}")
@@ -24,33 +23,3 @@ def capture():
             if log:
                 log_queue.put(f"Excepción en captura de imagen: {str(e)}")
         time.sleep(SleepTime)
-
-# def capture():
-#     while True:
-#         try:
-#             timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-#             filename = f"Pictures/{timestamp}.jpg"
-#             cmd = [
-#                 "libcamera-still",
-#                 "-o", filename,
-#                 "--width", "512",
-#                 "--height", "512",
-#                 "--nopreview"
-#             ]
-#             result = subprocess.run(
-#                 cmd,
-#                 stdout=subprocess.DEVNULL,
-#                 stderr=subprocess.PIPE,
-#                 text=True
-#             )
-#             if result.returncode != 0:
-#                 error_msg = result.stderr.strip()
-#                 if log:
-#                     log_queue.put(f"Error capturando imagen: {error_msg}")
-#             else:
-#                 if log:
-#                     log_queue.put(f"Imagen guardada: {filename}")
-#         except Exception as e:
-#             if log:
-#                 log_queue.put(f"Excepción en captura de imagen: {str(e)}")
-#         time.sleep(5)
