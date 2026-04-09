4ae2f6983b475af226abd500aac7d2883c32514d
Danylo <smolyakdanylo@gmail.com>
2026-04-09 13:20:22 +0100

    _

--- GroundStation/Server/static/js/app.js
+++ GroundStation/Server/static/js/app.js
@@ -245,7 +245,7 @@ for (const chart of charts) {
 }
 
 
-export const socket = io('http://localhost:5000', {
+export const socket = io('http://10.156.112.51:5000', {
 	reconnection: true,
 	reconnectionAttempts: Infinity,
 	reconnectionDelay: 1000,
