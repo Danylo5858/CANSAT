de10fbae54e9d377a29a1fb806ba287cb19e278f
Danylo5858 <smolyakdanylo@gmail.com>
2026-04-15 02:20:48 +0100

    _

--- Satellite/battery_controller.py
+++ Satellite/battery_controller.py
@@ -1,11 +1,11 @@
+import busio
 import board
 import adafruit_max1704x
 
-i2c = board.I2C()
+i2c = busio.I2C(board.SCL, board.SDA)
 
 max1704x = adafruit_max1704x.MAX17048(i2c)
 
 while True:
-    print(f"Voltaje: {max1704x.cell_voltage:.2f} V")
-    print(f"Batería: {max1704x.cell_percent:.1f} %")
-    print("----")
+    print(f"{max1704x.cell_percent:.1f}%")
+   
